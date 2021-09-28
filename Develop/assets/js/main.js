07057542037cd21d0b6e051c6c086da4777d9956
Mike Pizzica <mike.pizzica@gmail.com>
2021-09-28 14:38:48 -0400

    add all 5 questions and QA quiz through submission page which works as expected. clean up codebase / update preventdefault syntax which was previously incorrect

--- Develop/assets/js/main.js
+++ Develop/assets/js/main.js
@@ -16,7 +16,7 @@ function setTime() {
         timerH1.textContent = "Time: " + secondsLeft;
         //   timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
             // Calls function to create and append image
@@ -35,7 +35,7 @@ function start(){
     startH1.textContent = "Coding Quiz Challenge";
 
     var startP = document.createElement("p");
-    startP.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+    startP.textContent = "Try to answer the following five code-related questions within the time limit. Correct answers are worth 10 points. Incorrect answers will penalize your score/time by ten points/seconds!";
 
     var startButton = document.createElement("button");
     startButton.setAttribute("id","start-button");
@@ -68,22 +68,22 @@ function question1(){
     question1Button4.textContent = "numbers";
 
     question1Button1.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question2();
         wrong();
     });
     question1Button2.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question2();
         wrong();
     });
     question1Button3.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question2();
         correct();
     });
     question1Button4.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question2();
         wrong();
     });
@@ -106,22 +106,22 @@ function question2(){
     question2Button4.textContent = "square brackets";
 
     question2Button1.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question3();
         wrong();
     });
     question2Button2.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question3();
         wrong();
     });
     question2Button3.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question3();
         correct();
     });
     question2Button4.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         question3();
         wrong();
     });
@@ -146,24 +146,100 @@ function question3(){
     question3Button1.addEventListener("click", function(event){
         event.preventDefault();
         wrong();
-        end();
+        question4();
     });
     question3Button2.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
         wrong();
-        end();
+        question4();
     });
     question3Button3.addEventListener("click", function(event){
-        event.preventDefault
+        event.preventDefault();
+        wrong();
+        question4();
+    });
+    question3Button4.addEventListener("click", function(event){
+        event.preventDefault();
+        correct();
+        question4();
+    });
+    pageMiddle.append(question3H1, question3Button1, question3Button2, question3Button3, question3Button4);
+}
+
+function question4(){
+    console.log("question4")
+    pageMiddle.innerHTML = "";
+
+    var question4H1 = document.createElement("h1");
+    question4H1.textContent = "String values must be enclosed within _____ when being assigned to variables.";
+    var question4Button1 = document.createElement("button");
+    question4Button1.textContent = "commas";
+    var question4Button2 = document.createElement("button");
+    question4Button2.textContent = "curly brackets";
+    var question4Button3 = document.createElement("button");
+    question4Button3.textContent = "quotes";
+    var question4Button4 = document.createElement("button");
+    question4Button4.textContent = "parentheses";
+
+    question4Button1.addEventListener("click", function(event){
+        event.preventDefault();
+        wrong();
+        question5();
+    });
+    question4Button2.addEventListener("click", function(event){
+        event.preventDefault();
+        wrong();
+        question5();
+    });
+    question4Button3.addEventListener("click", function(event){
+        event.preventDefault();
+        correct();
+        question5();
+    });
+    question4Button4.addEventListener("click", function(event){
+        event.preventDefault();
+        wrong();
+        question5();
+    });
+    pageMiddle.append(question4H1, question4Button1, question4Button2, question4Button3, question4Button4);
+}
+
+function question5(){
+    console.log("question5")
+    pageMiddle.innerHTML = "";
+
+    var question5H1 = document.createElement("h1");
+    question5H1.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
+    var question5Button1 = document.createElement("button");
+    question5Button1.textContent = "JavaScript";
+    var question5Button2 = document.createElement("button");
+    question5Button2.textContent = "terminal/bash";
+    var question5Button3 = document.createElement("button");
+    question5Button3.textContent = "for loops";
+    var question5Button4 = document.createElement("button");
+    question5Button4.textContent = "console log";
+
+    question5Button1.addEventListener("click", function(event){
+        event.preventDefault();
         wrong();
         end();
     });
-    question3Button4.addEventListener("click", function(event){
-        event.preventDefault
+    question5Button2.addEventListener("click", function(event){
+        event.preventDefault();
+        wrong();
+        end();
+    });
+    question5Button3.addEventListener("click", function(event){
+        event.preventDefault();
+        wrong();
+        end();
+    });
+    question5Button4.addEventListener("click", function(event){
+        event.preventDefault();
         correct();
         end();
     });
-    pageMiddle.append(question3H1, question3Button1, question3Button2, question3Button3, question3Button4);
+    pageMiddle.append(question5H1, question5Button1, question5Button2, question5Button3, question5Button4);
 }
 
 function wrong(){
@@ -228,4 +304,3 @@ function end(){
 }
 
 start()
-
